e91c3eef71828cdecea20a133fa45897deb0e267
Dave Mason <dave.mason@ladbiblegroup.com>
2018-11-06 22:22:29 +0000

    Initial commit to build API that presents data from MySQL and MongoDB

---
+++ schema/index.js
@@ -0,0 +1,32 @@
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLNonNull
+} = require('graphql');
+
+const MeType = require('./types/me');
+const pgDb = require('../database/pgdb');
+
+const RootQueryType = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    me: {
+      type: MeType,
+      description: 'The current user identified by an api key',
+      args: {
+        key: {
+          type: new GraphQLNonNull(GraphQLString)
+        }
+      },
+      resolve: (obj, args, { pgPool }) => pgDb(pgPool).getUser(args.key)
+    }
+  }
+});
+
+const ncSchema = new GraphQLSchema({
+  query: RootQueryType
+  // mutation: ...
+});
+
+module.exports = ncSchema;
